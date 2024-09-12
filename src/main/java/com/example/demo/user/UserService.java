ed97d2e169cd473c8ca4c1178b6fba1bcec9714d
Leesamgsoo <sslee3680035@gmail.com>
2024-09-12 13:05:37 +0900

    password encode

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,19 +1,19 @@
 package com.example.demo.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void create(String username, String password, String email) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         user.setEmail(email);
         this.userRepository.save(user);
     }
